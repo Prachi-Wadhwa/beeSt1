26f1e0bd514e23296663ed8b899218a81a25ce29
Prachi-Wadhwa <101939726+Prachi-Wadhwa@users.noreply.github.com>
2023-12-02 11:24:51 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const router = require("./routes/bookRoutes");
 
 app.use(express.json());
 app.use("/books", router);
